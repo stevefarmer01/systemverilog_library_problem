d9de010b0b0f883c23e7be8178c276c2a4be3690
srfarmer1 <srfarmer1@not_a_real_email_address.dell_pro_max_14>
2026-03-23 12:06:06 +0000

    Added project_1_broken.tcl

--- README.md
+++ README.md
@@ -1,12 +1,67 @@
 
 # Introduction
 
-Welcome to my one of my only problems with SV.
+This a noddy design to prove the following....
 
 With VHDL you can compile files to different libraries in Vivado.  With V/SV you can't so they all go into work library and so there is no way they to differentiate 2 modules with the same name. Only OOC synth them to a netlist will work as Vivado does for IP by default.
 
 It's entirely possible you would have 2 modules of the same name with different code in a larger design.
 
-I too have renamed them in the past. Pain in the butt.
+This can be seen by the 2 files listed in 'set files [list \' in file ./design/vivado/project_1_broken.tcl
 
-When I get the time I will knock up a noddy design.
+    systemverilog_library_problem/domain_cross_slow_and_fast.sv
+
+..is always chosen over file..
+
+    broken/domain_cross_slow_and_fast.sv
+
+..no matter how you order this list. If 'broken/domain_cross_slow_and_fast.sv' was used it would cause './check_broken_build_the_same.sh' to fail as it would change the build output .bin file's md5sum.
+
+
+
+## To Build
+
+### In Termial
+
+    git clone https://github.com/stevefarmer01/systemverilog_library_problem.git
+    . /opt/Xilinx/Vivado/2019.2/settings64.sh
+    vivado -mode batch -source project_1.tcl
+    cd project_1
+    vivado project_1.xpr
+
+### In Vivado GUI
+
+    Generate Bitstream -> YES -> OK -> Cancel (when build finished)
+
+Wait for build to finish then..
+
+### In Termial
+
+    ./check_build_the_same.sh
+
+..if build output file '' is bit acurate then this should return
+
+    PASSES no_git FIRMWARE BUILD
+
+
+## To Build Broken
+
+    . /opt/Xilinx/Vivado/2019.2/settings64.sh
+    vivado -mode batch -source project_1_broken.tcl
+    cd project_1_broken
+    vivado project_1_broken.xpr
+
+### In Vivado GUI
+
+    Generate Bitstream -> YES -> OK -> Cancel (when build finished)
+
+Wait for build to finish then..
+
+### In Termial
+
+    ./check_broken_build_the_same.sh
+
+..if build output file '' is bit acurate then this should return
+
+    PASSES no_git FIRMWARE BUILD
+
---
+++ design/source/hdl/systemverilog/broken/domain_cross_slow_and_fast.sv
@@ -0,0 +1,65 @@
+/**
+ * @file    domain_cross_slow_and_fast.sv
+ * @author  Steve Farmer
+ * @date    07 July 2022
+ * @brief   This has an inverted added into data_out below to show that it's impossible to pick which SV source file to use if they have the same module name
+ * @note    
+ *
+ */
+
+`timescale 1ns / 1ps
+
+//If sync reset used then in Artix7 this appears to cause the registers to no longer be in the slice
+
+module domain_cross_slow_and_fast
+  #(
+    parameter INPUT_WIDTH = 1,
+    parameter NUMBER_OF_REGS = 2,
+    parameter FAST_TO_SLOW_DOMAIN_CROSSING = 0
+  )
+  (
+    input logic data_in_clk,    // This is always required even when not used in code below (FAST_TO_SLOW_DOMAIN_CROSSING = 0) because it is used for timing constraints in asscoiated file domain_cross_slow_and_fast.xdc
+    input logic data_out_clk,
+    //input logic reset,
+    input logic [INPUT_WIDTH-1 : 0] data_in,
+    output logic [INPUT_WIDTH-1 : 0] data_out = '0
+  );
+
+  logic [INPUT_WIDTH-1 : 0] shift_array_in = 0; // If this isn't initialed to 0 then line below with 'shift_array_in <= ~shift_array_in' in it won't work in behavioural simulation (initialises to 'X')
+  (* ASYNC_REG = "TRUE" *) logic [NUMBER_OF_REGS-1 : 0] [INPUT_WIDTH-1 : 0] data_shift_array = '0;
+
+
+  generate
+  //      logic data_in_r;
+  //      always_ff @(data_in_clk) data_in_r <= data_in;
+  //      always_comb shift_array_in = data_in_r;
+
+  //  if (FAST_TO_SLOW_DOMAIN_CROSSING == 0) begin
+        always_comb shift_array_in = data_in;
+  //  end
+  //  else begin
+  //      always_ff @(posedge data_in_clk) if (data_in) shift_array_in <= ~shift_array_in;
+  //  end
+  endgenerate
+
+  always_ff @ (posedge data_out_clk)
+      begin
+  //    if (reset === 1) begin
+  //        data_shift_array = '0;
+  //    end else begin
+      //        (* ASYNC_REG = "TRUE" *) reg [NUMBER_OF_REGS-1 : 0] data_shift_array; //This attribute doesn't appear to work in systemverliog on a variable but is does on a signal above
+          data_shift_array <= {data_shift_array[$left(data_shift_array)-1 : 0], shift_array_in};
+  //    end
+  end
+
+  generate
+  //  if (FAST_TO_SLOW_DOMAIN_CROSSING == 0) begin
+        always_comb data_out = ~data_shift_array[$left(data_shift_array)];
+  //  end
+  //  else begin
+  //      always_ff @(posedge data_out_clk) data_out <= data_shift_array[$left(data_shift_array)] ^ data_shift_array[$left(data_shift_array)-1];
+  //  end
+  endgenerate
+
+endmodule
+
